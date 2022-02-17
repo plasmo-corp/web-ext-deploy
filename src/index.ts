7f2347375a7b77dcd5a89be7c6b0a061f594ae44
Louis <6723574+louisgv@users.noreply.github.com>
2022-02-17 19:18:14 -0400

    Fix export structure

--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,16 @@
-import {
+export {
   ChromeOptions,
-  prepareToDeployChrome
+  prepareToDeployChrome as deployChrome
 } from "./stores/chrome/chrome-input";
-import { EdgeOptions, prepareToDeployEdge } from "./stores/edge/edge-input";
-import {
+export {
+  EdgeOptions,
+  prepareToDeployEdge as deployEdge
+} from "./stores/edge/edge-input";
+export {
   FirefoxOptions,
-  prepareToDeployFirefox
+  prepareToDeployFirefox as deployFirefox
 } from "./stores/firefox/firefox-input";
-import { OperaOptions, prepareToDeployOpera } from "./stores/opera/opera-input";
-
-export { OperaOptions, FirefoxOptions, EdgeOptions, ChromeOptions };
-
-export async function deployChrome(options: ChromeOptions): Promise<boolean> {
-  return prepareToDeployChrome(options);
-}
-
-export async function deployFirefox(options: FirefoxOptions): Promise<boolean> {
-  return prepareToDeployFirefox(options);
-}
-
-export async function deployEdge(options: EdgeOptions): Promise<boolean> {
-  return prepareToDeployEdge(options);
-}
-
-export async function deployOpera(options: OperaOptions): Promise<boolean> {
-  return prepareToDeployOpera(options);
-}
+export {
+  OperaOptions,
+  prepareToDeployOpera as deployOpera
+} from "./stores/opera/opera-input";
